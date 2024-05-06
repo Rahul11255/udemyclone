c4936f9ac434dfb5e5069ba3fc18ea3f2fdbc29f
Rahul Patel <rr710505@gmail.com>
2024-05-06 12:53:48 +0530

    fix some issue

---
+++ udemy/src/Components/home/landing/CardF.jsx
@@ -0,0 +1,31 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+const CardF = ({ watchimg, title }) => {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="150"
+          image={watchimg}
+          alt="card_img"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default CardF;
--- udemy/src/Components/home/landing/Feature.jsx
+++ udemy/src/Components/home/landing/Feature.jsx
@@ -1,11 +1,43 @@
-import "./feature.css"
+import CardF from "./CardF";
+import "./feature.css";
+import watchimg from "../../../assets/watch_png.png";
 
 const Feature = () => {
   return (
-    <div className="feature_container">
-    Feature
-    </div>
-  )
-}
+    <section className="feature_container">
+      <div className="feature_grid">
+        <div className="grid_item_left">
+          <img loading="lazy" src={watchimg} alt="watch_img" />
+        </div>
+        <div className="grid_item_right">
+          <CardF
+            watchimg={"https://m.media-amazon.com/images/I/41yGQGSDVfL.jpg"}
+            title="Build Quality and Design"
+          />
+          <CardF
+            watchimg={
+              "https://rajadigitalplanets.com/cdn/shop/products/31UHhMPnKES_d5fffb7a-85c7-47cf-8e21-a03100bda8c1_480x480@2x.jpg?v=1624256428"
+            }
+            title="Excellent battery life"
+          />
+          <CardF
+            watchimg={
+              "https://m.media-amazon.com/images/I/615jI2hiFRL._SX679_.jpg"
+            }
+            title="Accurate activity tracking"
 
-export default Feature
+          />
+          <CardF
+            watchimg={
+              "https://m.media-amazon.com/images/I/61a0cECYM8L._SX569_.jpg"
+            }
+            title="Bluetooth Calling"
+
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Feature;
