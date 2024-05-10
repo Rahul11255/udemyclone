95dd28b07ba497d0fdb657862babf2b51bf22e72
Rahul Patel <rr710505@gmail.com>
2024-05-10 19:24:46 +0530

    update

--- udemy/src/Components/auth/Logincomp.jsx
+++ udemy/src/Components/auth/Logincomp.jsx
@@ -7,7 +7,7 @@ const Logincomp = () => {
   return (
     <div className='register_contaainer'>
     <div className='register_img'>
-      <img src={loginimg} alt="" />
+      <img src={loginimg} alt="login-img" />
     </div>
     <div className='register_form_container'>
       <div>
