1c4d82c30d15fe264e3a851661ed7aafc5a47684
Rahul Patel <rr710505@gmail.com>
2024-05-21 10:35:49 +0530

    refactor emptycart code

--- udemy/src/Components/cart/EmptyCart.jsx
+++ udemy/src/Components/cart/EmptyCart.jsx
@@ -4,7 +4,7 @@ const EmptyCart = ({message}) => {
   return (
     <div className="emptycart_container">
       <h1>Your <span>Cart</span> Is Currently <span>Empty!</span> </h1>
-      <p>Continue browsing <Link to={'/product'} className="link">
+      <p>Continue browsing <Link to={'/products'} className="link">
          <span>here</span>
         </Link> 
        </p>
